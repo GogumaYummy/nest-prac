96b6979a87a74a64f25aa4f7bea2f585b1de3363
GogumaYummy <sora1211x@gmail.com>
2023-05-03 18:58:30 +0900

    feat: 환경 변수 유효성 검사 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { plainToInstance, Type } from 'class-transformer';
+import { IsDefined, IsInt } from 'class-validator';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
 
+class EnvConfig {
+  @IsDefined()
+  @IsInt()
+  @Type(() => Number)
+  PORT: number;
+}
 @Module({
-  imports: [ItemsModule, ConfigModule.forRoot({ envFilePath: '.env' })],
+  imports: [
+    ItemsModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      validate: (config) => plainToInstance(EnvConfig, config),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
